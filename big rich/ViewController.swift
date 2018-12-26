f3d467144f3762095fe86ba817f4cb2237d307c2
20161104567 <846129391@qq.com>
2018-12-26 10:42:54 +0800

    Initial Commit

---
+++ big rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  big rich
+//
+//  Created by 20161104573 on 2018/12/26.
+//  Copyright © 2018 20161104573. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
